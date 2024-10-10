c1ca5e541988b7639b5434b99241df02479b5ad4
Nanior <jakobspeybrouck@gmail.com>
2024-10-10 21:06:02 +0300

    Feat<07_termConversion>: Add round to 1 decimal point

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,10 +1,14 @@
 function convertToCelsius(fahrenheit) {
-  return (fahrenheit - 32) * 5 / 9, 1;
+  return roundToDecimal((fahrenheit - 32) * 5 / 9, 1);
 };
 
 function convertToFahrenheit(celsius) {
-  return celsius * 9 / 5 + 32, 1;
+  return roundToDecimal(celsius * 9 / 5 + 32, 1);
 };
+
+function roundToDecimal(number, decimal) {
+  return Math.round(number * 10 ** decimal) / 10 ** decimal;
+}
 
 // Do not edit below this line
 module.exports = {
