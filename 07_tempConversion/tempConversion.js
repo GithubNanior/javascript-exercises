6c430107622427da6f9644b7b0e0f588e9737dc2
Nanior <jakobspeybrouck@gmail.com>
2024-10-10 21:04:01 +0300

    Feat<07_termConversion>: Add conversion formulae between fahrenheit and celsius

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,9 @@
-const convertToCelsius = function() {
+function convertToCelsius(fahrenheit) {
+  return (fahrenheit - 32) * 5 / 9, 1;
 };
 
-const convertToFahrenheit = function() {
+function convertToFahrenheit(celsius) {
+  return celsius * 9 / 5 + 32, 1;
 };
 
 // Do not edit below this line
