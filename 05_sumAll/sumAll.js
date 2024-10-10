e2a01985e5657a61c138a4b9f53ba4a60e791c2a
Nanior <jakobspeybrouck@gmail.com>
2024-10-10 18:13:45 +0300

    Feat: Fulfilled test requirement 1 to 3

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,12 @@
-const sumAll = function(numbers) {
-    
+const sumAll = function(from, to) {
+    if (from < to)
+    {
+        return to * (to + 1) / 2 - from * (from - 1) / 2;
+    }
+    else
+    {
+        return from * (from + 1) / 2 - to * (to - 1) / 2;
+    }
 };
 
 // Do not edit below this line
