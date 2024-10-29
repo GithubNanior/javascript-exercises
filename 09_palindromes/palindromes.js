3eb85689a44fa5fe5e73b3ae21a98cb6577e0612
Nanior <jakobspeybrouck@gmail.com>
2024-10-29 21:14:41 +0200

    Feat<09_palindromes>: Implement function palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
+const palindromes = function (text) {
+    const cleanText = text.toLowerCase().split("").filter((char) => (char >= "a" && char <= "z") || (char >= "0" && char <= "9")).join("");
+    let halfCount = Math.floor(cleanText.length / 2);
 
+    return cleanText.substring(0, halfCount) === cleanText.substring(cleanText.length - halfCount).split("").reverse().join("");
 };
 
 // Do not edit below this line
