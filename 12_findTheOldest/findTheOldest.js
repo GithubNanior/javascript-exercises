54ccd27bebf47393c7d0ebf65c7ea4566effdd78
Nanior <jakobspeybrouck@gmail.com>
2024-10-29 23:00:29 +0200

    Feat<12_findTheOldest>: Implement findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,22 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    if (people.length == 0)
+    {
+        return undefined;
+    }
 
+    let oldestAge = 0;
+    let oldest;
+    people.forEach((person) => {
+        const age = (person.yearOfDeath === undefined ? new Date().getFullYear(): person.yearOfDeath) - person.yearOfBirth;
+        
+        if (age > oldestAge) 
+        {
+            oldestAge = age;
+            oldest = person;
+        }
+    });
+
+    return oldest;
 };
 
 // Do not edit below this line
