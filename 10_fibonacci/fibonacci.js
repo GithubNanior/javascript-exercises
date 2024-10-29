8fb1c45a01466c3080f668697405ed995c90ea5f
Nanior <jakobspeybrouck@gmail.com>
2024-10-29 21:40:05 +0200

    Feat<10_fibonacci>: Implement fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
+let cache = new Object();
 
+const fibonacci = function(number) {
+    if (number < 0) return "OOPS";
+    if (number == 0) return 0;
+    if (number == 1) return 1;
+
+    if (cache[number] === undefined)
+    {
+        cache[number] = fibonacci(number-1) + fibonacci(number-2);
+    }
+
+    return cache[number];
 };
 
 // Do not edit below this line
